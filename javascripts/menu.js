cea8e6c68b5a133094dfdd07a4a7cfb5636d92f2
Denis Z <denzel3330@gmail.com>
2018-12-04 00:25:53 +0300

    поменял названия всякие

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,16 +1,16 @@
 $( document ).ready(function() {
 
 $( ".cross" ).hide();
-$( ".menu" ).hide();
+$( ".menu__hamburger" ).hide();
 $( ".menu__button" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
+$( ".menu__hamburger" ).slideToggle( "slow", function() {
 $( ".menu__button" ).hide();
 $( ".cross" ).show();
 });
 });
 
 $( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
+$( ".menu__hamburger" ).slideToggle( "slow", function() {
 $( ".cross" ).hide();
 $( ".menu__button" ).show();
 });
